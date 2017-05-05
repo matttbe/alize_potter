47e7d79a3ea2927012a4a12f5d162e850c35d466
Nathan Chancellor <natechancellor@gmail.com>
2017-05-04 22:52:21 -0700

    arch: arm/arm64: silence some log spam
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -355,7 +355,7 @@ static void update_cpu_capacity(unsigned int cpu)
 
 	set_capacity_scale(cpu, cpu_capacity(cpu) / middle_capacity);
 
-	printk(KERN_INFO "CPU%u: update cpu_capacity %lu\n",
+	printk(KERN_DEBUG "CPU%u: update cpu_capacity %lu\n",
 		cpu, arch_scale_cpu_capacity(NULL, cpu));
 }
 
